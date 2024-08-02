f69ba8aa648236b98da3172982fc2c7491bcd950
alitaban <alitaban90@gmail.com>
2024-08-02 22:15:48 +0430

    feat: Authentication add UserEntity.java

---
+++ src/main/java/com/snapppay/tasks/expensetracker/domain/entities/AbstractAuditingEntity.java
@@ -0,0 +1,47 @@
+package com.snapppay.tasks.expensetracker.domain.entities;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+/**
+ * Base abstract class for entities which will hold definitions for created, last modified by and created,
+ * last modified by date.
+ */
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class AbstractAuditingEntity implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @CreatedBy
+    @Column(nullable = false, length = 50, updatable = false)
+    @JsonIgnore
+    private String createdBy;
+
+    @CreatedDate
+    @Column(nullable = false)
+    @JsonIgnore
+    private LocalDateTime createdDate;
+
+    @LastModifiedBy
+    @Column(length = 50)
+    @JsonIgnore
+    private String lastModifiedBy;
+
+    @LastModifiedDate
+    @JsonIgnore
+    private LocalDateTime lastModifiedDate;
+}
